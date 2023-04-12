20478439b84fdd849e8165f48af6b92a97b03ae2
Kundan <ntamsoy662gmail.com>
2023-04-13 01:22:13 +0530

    responsive navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,25 @@
 import React from 'react'
 export default function Nav(){
     return(
-        <nav>
-            <li><a href="/">Conversion</a></li>
-            <li><a href="/">Contact</a></li>
-            <li><a href="/">About</a></li>
+        <nav className="navbar navbar-expand-lg">
+            <a className="navbar-brand" href="#">Word Convertor</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Conversion</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
