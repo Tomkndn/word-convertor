37309ce69de8b345bab449863aa80cc568788e14
Kundan <ntamsoy662gmail.com>
2023-04-26 18:52:48 +0530

    added some more features

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ export default function Nav(){
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Conversion</a>
+                        <a className="nav-link Active" href="#">Conversion</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">About</a>
@@ -22,4 +22,4 @@ export default function Nav(){
             </div>
         </nav>
     );
-}
+};  
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
 
 export default function TextBox(props){
+
     let [text,setText] = useState('');
-    
+    let [text2,setText2] = useState('');
+
     const handleOnChange = (event)=>{
         setText(event.target.value);
     }
     const lowCase = ()=>{
         let newText = text.toLowerCase();
-        setText(newText)
+        setText2(newText)
     }
     const upCase = ()=>{
         let newText = text.toUpperCase();
-        setText(newText)
+        setText2(newText)
     }
     const firstChar =()=>{
         let newArray = text.split(' ');
@@ -21,15 +23,15 @@ export default function TextBox(props){
             let c = newArray[i][0].toUpperCase()+newArray[i].slice(1);
             newText.push(c);
         }
-        setText(newText.join(' '))
-    }
+        setText2(newText.join(' '))
+    } 
     const invert =()=>{
         let newArray = text.split(' ');
         let newText = [];
         for (let i = 0; i<newArray.length;i++){
             let word = "";
             for (let j = 0; j<newArray[i].length;j++){
-                if (newArray[i][j] == newArray[i][j].toUpperCase()){
+                if (newArray[i][j] === newArray[i][j].toUpperCase()){
                     let c = newArray[i][j].toLowerCase();
                     word = word + c;
                 }else{
@@ -39,31 +41,50 @@ export default function TextBox(props){
             }
             newText.push(word);
         }
-        setText(newText.join(' '))
+        setText2(newText.join(' '))
     }
     const clear = ()=>{
         let newText = '';
         setText(newText);
     }
+    const copy = ()=>{
+        let newText = text2;
+        setText(newText);
+    }
     return(
         <>
-        <form>
-          <div className="container mb-3">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.title}</label>
-          <textarea id='text' className="form-control" value={text} placeholder="text..." onChange={handleOnChange} rows="8"></textarea>
-          </div>
-        </form>
-        <div className='container button'>
-        <button type="button" className="btn btn-primary" onClick={lowCase}>lower</button>
-        <button type="button" className="btn btn-secondary" onClick={upCase}>UPPER</button>
-        <button type="button" className="btn btn-success" onClick={firstChar}>Fchar</button>
-        <button type="button" className="btn btn-danger" onClick={invert}>Invert</button>
-        <button type="button" className="btn btn-outline-warning" onClick={clear}>Clear</button>
-        </div>
-        <div className='container my-4'>
-            <h2>Your Text Summary:</h2>{text.split(' ').length} Words and {text.length} Characters.
-           <br/><br/><h2>Preview:</h2>{text}
-        </div>
+            <form>
+                <div className="container mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.title}</label>
+                    <textarea id='text' className="form-control" value={text} placeholder="text..." onChange={handleOnChange} rows="8"></textarea>
+                </div>
+            </form>
+            <div className='container buttons'>
+                <button type="button" className="btn btn-primary" onClick={lowCase}>lower</button>
+                <button type="button" className="btn btn-secondary" onClick={upCase}>UPPER</button>
+                <button type="button" className="btn btn-success" onClick={firstChar}>Fchar</button>
+                <button type="button" className="btn btn-danger" onClick={invert}>Invert</button>
+                <button type="button" className="btn btn-outline-warning" onClick={clear}>Clear</button>
+                <button type="button" class="btn btn-info" onClick={copy}>Copy</button>
+                <form className="input-group control">
+                    <div className="col-xs-3 m-2">
+                        <input type="text" className="form-control" id="text" placeholder="Enter color for Text" required/>
+                    </div>
+                    <div className="col-xs-3 m-2">
+                        <input type="text" className="form-control" id='bgcolor' placeholder="Enter color for BG" required/>
+                    </div>
+                <button type="button" className="btn btn-danger control">Submit</button>
+                </form>
+            </div>
+            <div className='container my-4'>
+                <h2>Your Text Summary:</h2>{text.split(' ').length} Words and {text.length} Characters.
+            </div>
+            <form>
+                <div className="container mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label"><h2>Preview:</h2></label>
+                    <textarea id='text' className="form-control preview" value={text2} placeholder="text..." rows="8"></textarea>
+                </div>
+            </form>
         </>
     );
-}
+};
